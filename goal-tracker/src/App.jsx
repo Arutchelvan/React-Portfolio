8f1e139a59b5f030ff8fa86c9fcf505afcdab44c
Arutchelvan <arulbalaji03@gmail.com>
2026-08-02 21:44:42 +0530

    Adding goal and updating on goal is is completed. Updating progress of each goal is in progress

--- goal-tracker/src/App.jsx
+++ goal-tracker/src/App.jsx
@@ -28,20 +28,46 @@ const initialGoal = [
 ];
 
 export default function App() {
+  const [goalList, setGoalList] = useState(initialGoal);
+  const [showAddGoal, setShowAddGoal] = useState(false);
+  const [selectedGoal, setSelectedGoal] = useState(null);
+
+  function handleGoalList(newGoal) {
+    console.log(newGoal);
+    setGoalList((goals) => [...goals, newGoal]);
+  }
+
+  function handleNewGoal() {
+    setShowAddGoal(!showAddGoal);
+  }
+
   return (
     <section className="mt-10">
       <div className="mx-auto max-w-7xl">
-        <div className="grid grid-cols-[30%_70%] gap-1">
+        <div className="grid sm:grid-cols-[40%_60%] lg:grid-cols-[30%_70%] lg:grid-row-1 sm:grid-row-auto gap-1">
           <aside className="p-2 rounded-xl border border-gray-200 bg-white p-6 shadow-sm">
             <div className="flex justify-between  mb-5">
               <h4 className="font-bold text-gray-900">My Goals</h4>
-              <Button>Add Goal +</Button>
+              <Button onClick={handleNewGoal}>
+                {showAddGoal ? `Close` : `Add Goal +`}
+              </Button>
             </div>
 
-            <GoalCardList />
+            <GoalCardList
+              onGoalList={goalList}
+              onSelectGoal={setSelectedGoal}
+            />
           </aside>
           <main className="p-2 rounded-xl border border-gray-200 bg-white p-6 shadow-sm">
-            <AddGoalForm />
+            {showAddGoal && (
+              <AddGoalForm
+                onFormSubmit={handleGoalList}
+                existingGoalList={goalList.length}
+                handleShowAddGoal={setShowAddGoal}
+              />
+            )}
+
+            <UpdateProgressForm goalList={goalList} />
           </main>
         </div>
       </div>
@@ -60,20 +86,22 @@ function Button({ children, onClick }) {
   );
 }
 
-function GoalCardList() {
-  const [goals, setGoals] = useState(initialGoal);
-
+function GoalCardList({ onGoalList, onSelectGoal }) {
   return (
     <div>
-      {goals.map((goal) => (
-        <GoalCard details={goal} key={goal.id} />
+      {onGoalList.map((goal) => (
+        <GoalCard details={goal} key={goal.id} onSelectGoal={onSelectGoal} />
       ))}
     </div>
   );
 }
 
-function GoalCard({ details }) {
+function GoalCard({ details, onSelectGoal }) {
   const percentage = (details.currentStatus / details.target) * 100;
+
+  function handleUpdateProgress() {
+    onSelectGoal(details.id);
+  }
 
   return (
     <div className="rounded-xl border border-green-400 bg-green-100 p-6 shadow-sm mb-5">
@@ -122,22 +150,53 @@ function GoalCard({ details }) {
           ></div>
         </div>
       </div>
-      <Button>Update Progress</Button>
+      <Button onClick={handleUpdateProgress}>Update Progress</Button>
     </div>
   );
 }
 
-function AddGoalForm() {
+function AddGoalForm({ onFormSubmit, existingGoalList, handleShowAddGoal }) {
+  const [goal, setGoal] = useState("");
+  const [tag, setTag] = useState("");
+  const [task, setTask] = useState("");
+  const [target, setTarget] = useState("");
+  const [currentStatus, setCurrentStatus] = useState("");
+
+  function handleForm(e) {
+    e.preventDefault();
+    if (!goal || !tag || !task || !target) return;
+
+    const id = existingGoalList + 1;
+    const newGoal = {
+      id,
+      goal,
+      tag,
+      task,
+      target,
+      currentStatus: !currentStatus ? 0 : currentStatus,
+    };
+    onFormSubmit(newGoal);
+    handleShowAddGoal(false);
+  }
   return (
-    <form className="flex flex-wrap">
+    <form className="flex flex-wrap" onSubmit={handleForm}>
+      <div className="mb-5 full">
+        <h2 className="text-4xl font-extrabold text-gray-900 mb-1">
+          Create New Goal
+        </h2>
+        <p className="text-base text-gray-700">
+          Define your next milestone and start tracking your progress.
+        </p>
+      </div>
       <div className="mb-5 w-full">
         <label className="text-sm font-medium text-gray-700 w-full block mb-2">
           Goal Title
         </label>
         <input
           type="text"
-          value={1}
+          value={goal}
           className="rounded-md border border-gray-300 bg-slate-100 px-3 py-3 text-sm focus:outline-indigo-600 block w-full"
+          onChange={(e) => setGoal(e.target.value)}
         />
       </div>
       <div className="mb-5 lg:w-1/2 md:w-full w-full lg:pr-2 md:pr-0">
@@ -146,8 +205,9 @@ function AddGoalForm() {
         </label>
         <input
           type="text"
-          value={1}
+          value={tag}
           className="rounded-md border border-gray-300 bg-slate-100 px-3 py-3 text-sm focus:outline-indigo-600 block w-full"
+          onChange={(e) => setTag(e.target.value)}
         />
       </div>
       <div className="mb-5 lg:w-1/2 md:w-full w-full lg:pl-2 md:pl-0">
@@ -156,8 +216,9 @@ function AddGoalForm() {
         </label>
         <input
           type="text"
-          value={1}
+          value={task}
           className="rounded-md border border-gray-300 bg-slate-100 px-3 py-3 text-sm focus:outline-indigo-600 block w-full"
+          onChange={(e) => setTask(e.target.value)}
         />
       </div>
       <div className="mb-5 lg:w-1/2 md:w-full w-full lg:pr-2 md:pr-0">
@@ -165,9 +226,12 @@ function AddGoalForm() {
           Target
         </label>
         <input
-          type="text"
-          value={1}
+          type="number"
+          value={target}
           className="rounded-md border border-gray-300 bg-slate-100 px-3 py-3 text-sm focus:outline-indigo-600 block w-full"
+          onChange={(e) =>
+            setTarget(Number(e.target.value) > 0 && Number(e.target.value))
+          }
         />
       </div>
       <div className="mb-5 lg:w-1/2 md:w-full w-full lg:pl-2 md:pl-0">
@@ -175,12 +239,58 @@ function AddGoalForm() {
           Current Status
         </label>
         <input
-          type="text"
-          value={1}
+          type="number"
+          value={currentStatus}
           className="rounded-md border border-gray-300 bg-slate-100 px-3 py-3 text-sm focus:outline-indigo-600 block w-full"
+          onChange={(e) =>
+            setCurrentStatus(
+              Number(e.target.value) <= target && Number(e.target.value) >= 0
+                ? Number(e.target.value)
+                : currentStatus,
+            )
+          }
         />
       </div>
-      <Button>Create A Goal</Button>
+      <div className="flex h-[40px] gap-5">
+        <Button>Cancel</Button>
+        <Button>Create A Goal</Button>
+      </div>
+    </form>
+  );
+}
+
+function UpdateProgressForm({ goalList }) {
+  function handleForm() {}
+  return (
+    <form className="flex flex-wrap" onSubmit={handleForm}>
+      <div className="mb-5 full">
+        <h2 className="text-4xl font-extrabold text-gray-900 mb-1">
+          Update Your Current Status of xxxxxxxxcurrentActiveTitle
+        </h2>
+        <p className="text-base text-gray-700">
+          Your target on this goal is xxxxxxxtarget
+        </p>
+      </div>
+      <div className="mb-5 w-full lg:pl-2 md:pl-0">
+        <label className="text-sm font-medium text-gray-700 w-full block mb-2">
+          Current Status
+        </label>
+        <input
+          type="number"
+          value={currentStatus}
+          className="rounded-md border border-gray-300 bg-slate-100 px-3 py-3 text-sm focus:outline-indigo-600 block w-full"
+          onChange={(e) =>
+            setCurrentStatus(
+              Number(e.target.value) <= target && Number(e.target.value) >= 0
+                ? Number(e.target.value)
+                : currentStatus,
+            )
+          }
+        />
+      </div>
+      <div className="flex h-[40px] gap-5">
+        <Button>Update</Button>
+      </div>
     </form>
   );
 }
